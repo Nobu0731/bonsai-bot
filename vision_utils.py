08823952dee71f5ba51bc6b2f3a9afbc3748255e
Nobu0731 <amano@amari-group.com>
2025-03-28 10:34:07 +0900

    Update vision_utils.py

--- vision_utils.py
+++ vision_utils.py
@@ -1,1 +1,10 @@
+import os
+from google.cloud import vision
 
+def analyze_image_from_bytes(image_bytes: bytes) -> list:
+    client = vision.ImageAnnotatorClient()
+    image = vision.Image(content=image_bytes)
+    response = client.label_detection(image=image)
+    if response.error.message:
+        raise Exception(f"Vision API error: {response.error.message}")
+    return [label.description for label in response.label_annotations]
